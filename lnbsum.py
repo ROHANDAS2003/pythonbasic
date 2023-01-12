7446799b3d499e73c30e78f03b8dd91a734bce19
Rohan das <110011068+ROHANDAS2003@users.noreply.github.com>
2023-01-12 17:47:52 +0530

    Initial commit

---
+++ lnbsum.py
@@ -0,0 +1,14 @@
+num = [0,0,0,0,0]
+for i in range(0,5):
+    print("Enter number", i+1)
+    num[i] = int(input())
+
+sum = 0
+for i in range(0,5):
+    if num[i]>9:
+        sum = sum + num[i]
+
+if sum == 0:
+    print("The the numbers were less than 9")
+else:
+    print("The sum of numbers greater than 9 is: ", sum)
