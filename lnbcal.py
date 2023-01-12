359ca695cd45595feae7a6263ab80a39e169a917
Rohan das <110011068+ROHANDAS2003@users.noreply.github.com>
2023-01-12 17:53:03 +0530

    Create lnbcal.py

---
+++ lnbcal.py
@@ -0,0 +1,7 @@
+first = int(input("Enter first number:\n"))
+second = int(input("Enter second number:\n"))
+
+if (first*second)>500:
+    print("The sum is: ", first+second)
+else:
+    print("Hello LNB code is running fine !!")
